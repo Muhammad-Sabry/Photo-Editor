b013b01ede9cec63aea8a8c67ff6929b5dbcd274
Muhammad-Sabry <muhamad.sabry7@gmail.com>
2022-07-06 19:27:45 +0200

    Initiate the Project

--- src/pages/photo-editor.js
+++ src/pages/photo-editor.js
@@ -5,11 +5,41 @@ import usePanZoom from "use-pan-and-zoom";
 import "../styles/photo-editor.scss";
 
 const PhotoEditor = () => {
-  const { transform, panZoomHandlers, setContainer } = usePanZoom({});
-
+  const { transform, panZoomHandlers, setContainer, container, setZoom, setPan } = usePanZoom({
+    minX: 0-(document.body.clientWidth * 0.1),
+    maxX: document.body.clientWidth * 0.1,
+    minY: 0-(document.body.clientHeight * 0.1),
+    maxY: document.body.clientHeight * 0.1,
+    enableZoom: false,
+  });
+  
   const onDrop = useCallback((droppedFiles) => {
-    //
-  }, []);
+    droppedFiles.forEach(file => {
+      const reader = new FileReader();
+
+      // Reading the image onload event to extract its data and assign it to a new instant of Image()
+      reader.onload = (file) => {
+        const img = new Image()
+
+        //Reading the image width & height when it's uploaded
+        img.src = file.target.result
+        img.onload = () => {
+          const widthRatio = container.offsetWidth/img.width,
+          heightRatio = container.offsetHeight/img.height,
+          heightCenter = ((container.firstChild.offsetHeight * Math.max(widthRatio, heightRatio)) - container.offsetHeight)/4
+
+          // Set the image zoom and pan according to the container div dimentions
+          if(widthRatio > 1 || heightRatio > 1) {
+            setZoom(Math.max(widthRatio, heightRatio));
+            setPan({x: 0, y: heightCenter});
+          } else {
+            setZoom(1);
+          }
+        }
+      }
+      reader.readAsDataURL(file)
+    })
+  }, [container, setZoom, setPan]);
 
   const { acceptedFiles, getRootProps, getInputProps, isDragActive } =
     useDropzone({
